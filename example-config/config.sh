dea2f7717b4e5dae0e449f23ece692e28f085fef
Ask Sødal <asksodal@gmail.com>
2023-10-18 02:42:57 +0200

    added a main config file

---
+++ example-config/config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+infonator-set script-path battery-percentage example-config/battery-percentage.sh
+infonator-set script-path battery-time-left example-config/battery-time-left.sh 
+infonator-set script-path brightness example-config/brightness.sh 
+infonator-set script-path cpu-temperature example-config/cpu-temperature.sh
+infonator-set script-path date example-config/date.sh
+infonator-set script-path ram-usage example-config/ram-usage.sh 
+infonator-set script-path time example-config/time.sh 
+infonator-set script-path volume example-config/volume.sh
+infonator-set script-path wifi-name example-config/wifi-name.sh 
+infonator-set script-path wifi-quality example-config/wifi-quality.sh
